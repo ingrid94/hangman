8fd3eb59ead6535ac0f0e45e1c7f51611b7da3e3
Ingrid <ingrid@dev.noom.ee>
2017-07-13 15:52:05 +0300

    Initial commit

---
+++ src/Hangman.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ingrid on 13.07.2017.
+ */
+public class Hangman {
+}
---
+++ src/HangmanTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by ingrid on 13.07.2017.
+ */
+class HangmanTest {
+    @Test
+    void takeWordTest(){
+        assertEquals("Test", "Test");
+    }
+}
